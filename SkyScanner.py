a2e2e6ae33606306f590e1a0830720d6a1f51e0a
S1rK <talnakar5@gmail.com>
2020-01-25 18:43:30 +0200

    Added full documentations to the functions in SkyScanner.

--- SkyScanner.py
+++ SkyScanner.py
@@ -1,17 +1,16 @@
 import http.client
 from time import sleep
 import json
-from typing import List
 from numpy import array
 
-# from SkyScanner.MachineLearning import learn
+from MachineLearning import learn
 
 
 def get_session_key(country='US', currency='USD', locale='en-US', originPlace='SFO-sky',
                     destinationPlace='LHR-sky', outboundDate='2020-02-01', adults='1',
                     inboundDate='2020-02-10', cabinClass='business', children='0', infants='0') -> str:
     """
-    Send POST msg to server tog et a session key.
+    Sends POST msg to server to get a session key.
     MUST
     :param country: The market/country your user is in (see docs for list of markets).
     :param currency: The currency you want the prices in (3-letter currency code).
@@ -67,7 +66,9 @@ def poll_session_results(session_key: str, sortType='price', sortOrder='asc', du
     :param duration: Filter for maximum duration in minutes. Integer between 0 and 1800.
     :param pageIndex: The desired page number. Leave empty for no pagination.
     :param pageSize: The number of itineraries per page. Defaults to 10 if not specified.
-    :return: A list of list of 3 items: price (int), InboundLegId (str), OutboundLegId (str).
+    :return: A vector (np.array) of fights. Each flight is a vector (np.array) of 5 items:
+             minimum price of the flight (float), departure time (str "yyyy-mm-ddThh:mm:ss"),
+             arrival time (str "yyyy-mm-ddThh:mm:ss"), duration of the flight (int), number of stops (int)
     """
     # create a connection
     conn = http.client.HTTPSConnection("skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
@@ -103,40 +104,80 @@ def poll_session_results(session_key: str, sortType='price', sortOrder='asc', du
     flights = data_j["Itineraries"]
     legs = data_j["Legs"]
 
-    print(json.dumps(legs, indent=4, sort_keys=True))
-
     # create the flights properties list
     flights_properties = array([array(
-        [flight["PricingOptions"][0]["Price"], leg["Departure"], leg["Arrival"], leg["Duration"], len(leg["Stops"])])
-                                for flight, leg in zip(flights, legs)])
+        [float(flight["PricingOptions"][0]["Price"]), leg["Departure"], leg["Arrival"], int(leg["Duration"]),
+         int(len(leg["Stops"]))]) for flight, leg in zip(flights, legs)])
 
     return flights_properties
 
 
-def default_poll_session_results() -> array:
+def get_flights(country='US', currency='USD', locale='en-US', originPlace='SFO-sky',
+                         destinationPlace='LHR-sky', outboundDate='2020-02-01', adults='1',
+                         inboundDate='2020-02-10', cabinClass='business', children='0', infants='0',
+                         sortType='price', sortOrder='asc', duration='1800', pageIndex='0',
+                         pageSize='100') -> array:
+    """
+    Sends a POST msg to server to get a session key (with the given parameters). Then, sends a GET Poll session results
+    request, and returns the flights in a vector.
+
+    REQUIRED for getting a session key
+    :param country: The market/country your user is in (see docs for list of markets).
+    :param currency: The currency you want the prices in (3-letter currency code).
+    :param locale: The locale you want the results in (ISO locale).
+    :param originPlace: The origin place (see docs for places).
+    :param destinationPlace: The destination place (see docs for places).
+    :param outboundDate: The outbound date. Format “yyyy-mm-dd”.
+    :param adults: Number of adults (16+ years). Must be between 1 and 8.
+
+    OPTIONAL for getting a session key
+    :param inboundDate: The return date. Format “yyyy-mm-dd”. Use empty string for oneway trip.
+    :param cabinClass: The cabin class. Can be “economy”, “premiumeconomy”, “business”, “first”.
+    :param children: Number of children (1-16 years). Can be between 0 and 8.
+    :param infants: Number of infants (under 12 months). Can be between 0 and 8.
+
+
+    OPTIONAL for getting the session's results
+    :param sortType: The parameter to sort results on. Can be carrier, duration, outboundarrivetime, outbounddeparttime,
+                     inboundarrivetime, inbounddeparttime, price*.
+    :param sortOrder: The sort order. ‘asc’ or 'desc’.
+    :param duration: Filter for maximum duration in minutes. Integer between 0 and 1800.
+    :param pageIndex: The desired page number. Leave empty for no pagination.
+    :param pageSize: The number of itineraries per page. Defaults to 10 if not specified.
+
+
+    :return: A vector (np.array) of fights. Each flight is a vector (np.array) of 5 items:
+             minimum price of the flight (float), departure time (str "yyyy-mm-ddThh:mm:ss"),
+             arrival time (str "yyyy-mm-ddThh:mm:ss"), duration of the flight (int), number of stops (int)
+    """
     # getting the session's key
     key = ""
     while key == "":
         try:
-            key = get_session_key()
+            key = get_session_key(country=country, currency=currency, locale=locale, originPlace=originPlace,
+                                  destinationPlace=destinationPlace, outboundDate=outboundDate, adults=adults,
+                                  inboundDate=inboundDate, cabinClass=cabinClass, children=children, infants=infants)
             print(f"got a key: {key}.")
         except AssertionError as e:
             print(str(e))
             print("Failed getting a key. Trying again in 1 sec.")
+            print("------------------------------------------------------------------------------------------------")
             sleep(1)
+
     # getting the session's results
-    # key = "2b096243-5ea9-4a3d-b8ef-0dccefba4082"
     results = None
     while results is None:
         try:
-            return poll_session_results(key)
+            return poll_session_results(session_key=key, sortType=sortType, sortOrder=sortOrder, duration=duration,
+                                        pageIndex=pageIndex, pageSize=pageSize)
         except AssertionError as e:
             print(str(e))
-            print("Failed getting session's results. Trying to get another key.")
+            print("Failed getting session's results. Trying again.")
+            print("------------------------------------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
     # learn(0)
-    flights_ = default_poll_session_results()
+    flights_ = get_flights()
     print(flights_)
 
