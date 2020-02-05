c9bba75c0b822851bf8e5217145ad0e522799f4e
shilo-hub <60120500+shilo-hub@users.noreply.github.com>
2020-02-05 15:09:03 +0200

    correct prediction and learn

--- NewMachineLearning.py
+++ NewMachineLearning.py
@@ -16,11 +16,11 @@ from typing import Callable
 
 features = len(attributes)
 input = 2 * features
-epoch = 10
+epoch = 3
 learn_feature = 0
 
 
-def rank_by_predicted_comparator(flights: np.array, comparator: Callable[[np.array, np.array], int]):
+def rank_by_predicted_comparator(flights: np.array, predict: Callable[[np.array, np.array], int]):
     """
 
     :param flights:
@@ -31,7 +31,8 @@ def rank_by_predicted_comparator(flights: np.array, comparator: Callable[[np.arr
     flights_ranks = [0 for _ in range(length)]
     for i in range(0, length):
         for j in range(i, length):
-            if comparator(flights[i], flights[j]) == 0:
+            x = np.concatenate([flights[i], flights[j]], axis=0)
+            if predict(x) == 0:
                 flights_ranks[i] += 1
             else:
                 flights_ranks[j] += 1
@@ -70,8 +71,8 @@ def price_per_duration_automatic_user(flight1: np.array, flight2: np.array) -> i
     :param flight2: second flight.
     :return: 0 if it prefer flight1, 1 if it prefer flight2.
     """
-    f1 = (1 + flight1[2]) * flight1[0] / flight1[1]
-    f2 = (1 + flight2[2]) * flight2[0] / flight2[1]
+    f1 = (1 + flight1[2]) * flight1[1] + flight1[0]
+    f2 = (1 + flight2[2]) * flight2[1] + flight2[0]
     return int(f1 > f2)
 
 
@@ -98,6 +99,7 @@ class net(nn.Module):
         self.fc3 = nn.Linear(int(input / 2), 2)
 
     def forward(self, x):
+        x = torch.FloatTensor(x)
         x = x.view(-1, self.features)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -121,55 +123,71 @@ class net(nn.Module):
     def ranking_flights(self, flights):
         return rank_by_predicted_comparator(flights, self.prediction)
 
+    def from_rank_to_train_set(self, flights):
+        data_to_learn = np.ndarray((0, features * 2))
+        labels = np.ndarray((0, 1), dtype=int)
+        for i in range(0, len(flights)):
+            for j in range(i + 1, len(flights)):
+                if i != j:
+                    c1 = np.concatenate([flights[i], flights[j]], axis=0)
+                    data_to_learn = np.append(data_to_learn, [c1], axis=0)
+                    labels = np.append(labels, [[1]], axis=0)
+                    c2 = np.concatenate([flights[j], flights[i]], axis=0)
+                    data_to_learn = np.append(data_to_learn, [c2], axis=0)
+                    labels = np.append(labels, [[0]], axis=0)
+        data_to_learn = torch.FloatTensor(data_to_learn)
+        labels = torch.IntTensor(labels)
+        labels = labels.to(dtype=torch.int64)
+        training_set = [(x, y) for x, y in zip(data_to_learn, labels)]
+        return training_set
+
 
 class SVM(object):
-    def __init__(self, eta=0.01, lambd=0.075):
+    def __init__(self, _eta=0.5, lambd=0.5):
         self.num_of_class = 2
-        self.__w = np.zeros((self.num_of_class, input))
-        self.__b = np.zeros(self.num_of_class)
-        for i in range(self.num_of_class):
-            self.__b[i] = 1.0
-        self.__eta = eta
-        self.__lambda = lambd
+        self.w = np.zeros((self.num_of_class, input))
+        self.b = np.zeros(self.num_of_class)
+        self.eta = _eta
+        self._lambda = lambd
 
     def train_svm(self, training_set):
         for x, y in training_set:
             y_hat = int(self.prediction(x))
-            self.__w[y, :] = (1 - self.__eta * self.__lambda) * self.__w[y, :] + self.__eta * x
-            self.__w[y_hat, :] = (1 - self.__eta * self.__lambda) * self.__w[y, :] - self.__eta * x
-            self.__b[y] = self.__b[y] + self.__eta
-            self.__b[y_hat] = self.__b[y_hat] - self.__eta
+            self.w[y, :] = (1 - self.eta * self._lambda) * self.w[y, :] + self.eta * x
+            self.w[y_hat, :] = (1 - self.eta * self._lambda) * self.w[y_hat, :] - self.eta * x
+            self.b[y] = (1 - self.eta * self._lambda) * self.b[y] + self.eta
+            self.b[y_hat] = (1 - self.eta * self._lambda) * self.b[y_hat] - self.eta
+
             for i in range(self.num_of_class):
                 if i != y and i != y_hat:
-                    self.__w[i, :] = (1 - self.__eta * self.__lambda) * self.__w[i, :]
+                    self.w[i, :] = (1 - self.eta * self._lambda) * self.w[i, :]
+
+        print(self.w)
+        print(self.b)
 
     def prediction(self, x):
-        res = np.dot(self.__w, np.transpose(x))
+        res = np.dot(self.w, np.transpose(x))
         for i in range(self.num_of_class):
-            res[i] = res[i] + self.__b[i]
+            res[i] = res[i] + self.b[i]
         return np.argmax(res)
 
     def ranking_flights(self, flights):
         return rank_by_predicted_comparator(flights, self.prediction)
 
-
-def from_rank_to_train_set(flights):
-    data_to_learn = np.ndarray((0, features * 2))
-    labels = np.ndarray((0, 1), dtype=int)
-    for i in range(0, len(flights)):
-        for j in range(i + 1, len(flights)):
-            if i != j:
-                c1 = np.concatenate([flights[i], flights[j]], axis=0)
-                data_to_learn = np.append(data_to_learn, [c1], axis=0)
-                labels = np.append(labels, [[1]], axis=0)
-                c2 = np.concatenate([flights[j], flights[i]], axis=0)
-                data_to_learn = np.append(data_to_learn, [c2], axis=0)
-                labels = np.append(labels, [[0]], axis=0)
-    data_to_learn = torch.FloatTensor(data_to_learn)
-    labels = torch.IntTensor(labels)
-    labels = labels.to(dtype=torch.int64)
-    training_set = [(x, y) for x, y in zip(data_to_learn, labels)]
-    return training_set
+    def from_rank_to_train_set(self, flights):
+        data_to_learn = np.ndarray((0, features * 2))
+        labels = np.ndarray((0, 1), dtype=int)
+        for i in range(0, len(flights)):
+            for j in range(i + 1, len(flights)):
+                if i != j:
+                    c1 = np.concatenate([flights[i], flights[j]], axis=0)
+                    data_to_learn = np.append(data_to_learn, [c1], axis=0)
+                    labels = np.append(labels, [[1]], axis=0)
+                    c2 = np.concatenate([flights[j], flights[i]], axis=0)
+                    data_to_learn = np.append(data_to_learn, [c2], axis=0)
+                    labels = np.append(labels, [[0]], axis=0)
+        training_set = [(x, y) for x, y in zip(data_to_learn, labels)]
+        return training_set
 
 
 def evaluate_ranking(real_ranking, predicted_ranking):
@@ -180,17 +198,19 @@ def evaluate_ranking(real_ranking, predicted_ranking):
 
 
 def evaluate_model_by_auto_user(validation_set: np.array, auto_user_comparator: Callable[[np.array, np.array], int],
-                                model_comparator: Callable[[np.array, np.array], int]) -> float:
+                                model_prediction: Callable[[np.array, np.array], int]) -> float:
     length = len(validation_set)
     success = 0
     num_of_iter = 0
     for i in range(length):
         flight1 = flights[i]
-        for j in range(i+1, length):
+        for j in range(i + 1, length):
             flight2 = flights[j]
-            if auto_user_comparator(flight1, flight2) == model_comparator(flight1, flight2):
+            x1 = np.concatenate([flight1, flight2], axis=0)
+            x2 = np.concatenate([flight2, flight1], axis=0)
+            if auto_user_comparator(flight1, flight2) == model_prediction(x1):
                 success += 1
-            if auto_user_comparator(flight2, flight1) == model_comparator(flight2, flight1):
+            if auto_user_comparator(flight2, flight1) == model_prediction(x2):
                 success += 1
             num_of_iter += 1
     return success / (2 * num_of_iter)
@@ -199,7 +219,7 @@ def evaluate_model_by_auto_user(validation_set: np.array, auto_user_comparator: 
 def learn(flights: np.array):
     num_of_flights = len(flights)
 
-    good_ranking_flights = ranking_flights_by_automatic_user(flights, sum_automatic_user)
+    good_ranking_flights = ranking_flights_by_automatic_user(flights, price_per_duration_automatic_user)
 
     _net = net()
     optimizer = optim.SGD(_net.parameters(), lr=0.01)
@@ -210,20 +230,28 @@ def learn(flights: np.array):
         svm_rank = _svm.ranking_flights(flights)
         net_rank = _net.ranking_flights(flights)
 
-        svm_top_5 = ranking_flights_by_automatic_user(svm_rank[0 - 5], sum_automatic_user)
-        net_top_5 = ranking_flights_by_automatic_user(net_rank[0 - 5], sum_automatic_user)
+        svm_top_5 = ranking_flights_by_automatic_user(svm_rank[:5], price_per_duration_automatic_user)
+        net_top_5 = ranking_flights_by_automatic_user(net_rank[:5], price_per_duration_automatic_user)
 
         # TODO: put here method for evaluation the ranking
         """
         SHOULD WE REALLY EVALUATE ONLY BY RANKING, AND NOT BY PARIS AND THEIR COMPARING?
         """
+        svm_eval = evaluate_model_by_auto_user(flights, sum_automatic_user, _svm.prediction)
+        net_eval = evaluate_model_by_auto_user(flights, sum_automatic_user, _net.prediction)
+
+        print("---------svm recommend-----------")
+        print("\n".join([flight_to_string(f) for f in svm_rank[:5]]))
+        print(svm_eval)
+        print("---------net recommend-----------")
+        print("\n".join([flight_to_string(f) for f in net_rank[:5]]))
+        print(net_eval)
+
         # svm_eval = evaluate_ranking(good_ranking_flights, svm_top_5)
         # net_eval = evaluate_ranking(good_ranking_flights, net_top_5)
-        svm_eval = evaluate_model_by_auto_user(flights[5:], sum_automatic_user, _svm.prediction)
-        net_eval = evaluate_model_by_auto_user(flights[5:], sum_automatic_user, _net.prediction)
 
-        svm_set_train = from_rank_to_train_set(svm_top_5)
-        net_set_train = from_rank_to_train_set(net_top_5)
+        svm_set_train = _svm.from_rank_to_train_set(svm_top_5)
+        net_set_train = _net.from_rank_to_train_set(net_top_5)
 
         _svm.train_svm(svm_set_train)
         _net.train_net(optimizer, net_set_train)
@@ -233,13 +261,18 @@ def learn(flights: np.array):
 
 if __name__ == "__main__":
     from SkyScanner import get_flights
+
     flights = get_flights()
     print(f"got {len(flights)} flights")
     np.random.shuffle(flights)
-    fs = flights[:5]
-    fs2 = fs
-    ranked = ranking_flights_by_automatic_user(fs, sum_automatic_user)
-    print("\n".join([flight_to_string(f) for f in ranked]))
-    print("--------------------------")
-    ranked = ranking_flights_by_automatic_user(fs2, greedy_automatic_user)
-    print("\n".join([flight_to_string(f) for f in ranked]))
+
+    learn(flights)
+
+    # fs = flights[:20]
+    # fs2 = fs
+    # print("---------result-----------")
+    # ranked = ranking_flights_by_automatic_user(fs, price_per_duration_automatic_user)
+    # print("\n".join([flight_to_string(f) for f in ranked]))
+    # print("--------------------------")
+    # ranked = ranking_flights_by_automatic_user(fs2, greedy_automatic_user)
+    # print("\n".join([flight_to_string(f) for f in ranked]))
